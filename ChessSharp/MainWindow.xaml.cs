a0b50fa030fd0142c7a0edd2c6d76a5192b71aa1
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2024-12-07 12:10:29 -0400

    ui looks good now but the chess board is still funky

--- ChessSharp/MainWindow.xaml.cs
+++ ChessSharp/MainWindow.xaml.cs
@@ -1,24 +1,23 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System;
+using System.Globalization;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChessSharp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public class MinValueConverter : IValueConverter
     {
-        public MainWindow()
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            InitializeComponent();
+            if (value is double width && parameter is string heightString && double.TryParse(heightString, out double height))
+            {
+                return Math.Min(width, height);
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
